946d91464917e82b08ef5ab7c2a2d4d6869234cb
Peerchemist <dx.1178@gmail.com>
2018-03-21 14:00:40 +0100

    rework entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 set -e
 
+if [[ ! -s "$PPC_DATA/peercoin.conf" ]]; then
+	cat <<-EOF > "$PPC_DATA/peercoin.conf"
+	rpcallowip=::/0
+	rpcpassword=${RPC_PASSWORD}
+	rpcuser=${RPC_USER}
+	EOF
+	chown sunny:sunny "$PPC_DATA/peercoin.conf"
+fi
+
 if [[ "$1" == "peercoin-cli" || "$1" == "peercoind" || "$1" == "ppcoind" ]]; then
-	mkdir -p "$PPC_DATA"
-
-	if [[ ! -s "$PPC_DATA/peercoin.conf" ]]; then
-		cat <<-EOF > "$PPC_DATA/peercoin.conf"
-		rpcallowip=::/0
-		rpcpassword=${RPC_PASSWORD}
-		rpcuser=${RPC_USER}
-		EOF
-		chown sunny:sunny "$PPC_DATA/peercoin.conf"
-	fi
-
-	# ensure correct ownership and linking of data directory
-	# we do not update group ownership here, in case users want to mount
-	# a host directory and still retain access to it
-	chown -R sunny "$PPC_DATA"
-	ln -sfn "$PPC_DATA" /home/sunny/.peercoin
-	chown -h sunny:sunny /home/sunny/.peercoin
 
 	exec gosu sunny "$@"
 fi

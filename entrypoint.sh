791ec46bb123e1365f82387e751d5dc674780565
Peerchemist <dx.1178@gmail.com>
2018-03-21 21:40:57 +0100

    rework entrypoint.sh to cover more functionality.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 set -e
 
-if [[ ! -s "$PPC_DATA/peercoin.conf" ]]; then
-	cat <<-EOF > "$PPC_DATA/peercoin.conf"
-	rpcallowip=::/0
-	rpcpassword=${RPC_PASSWORD}
-	rpcuser=${RPC_USER}
-	EOF
-	chown sunny:sunny "$PPC_DATA/peercoin.conf"
+if [ $(echo "$1" | cut -c1) = "-" ]; then
+  	echo "$0: assuming arguments for peercoind"
+  	set -- peercoind "$@"
 fi
 
-if [[ "$1" == "peercoin-cli" || "$1" == "peercoind" || "$1" == "ppcoind" ]]; then
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "peercoind" ] || [ "$1" = "peercoind" ]; then
+	if [[ ! -s "$PPC_DATA/peercoin.conf" ]]; then
+		cat <<-EOF > "$PPC_DATA/peercoin.conf"
+		rpcallowip=::/0
+		rpcpassword=${RPC_PASSWORD}
+		rpcuser=${RPC_USER}
+		EOF
+		chown sunny:sunny "$PPC_DATA/peercoin.conf"
+	fi
+  	set -- "$@" -datadir="$PPC_DATA"
+fi
 
-	exec gosu sunny "$@"
+if [ "$1" = "peercoind" ] || [ "$1" = "peercoin-cli" ] || [ "$1" = "ppcoind" ]; then
+  	echo
+  	exec gosu sunny "$@"
 fi
 
 exec "$@"
